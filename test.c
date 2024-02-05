1f1914cd8bb744a41c26f89c2688376592b2c4b1
sokaraku <soner.karakus1999@gmail.com>
2024-02-05 14:40:33 +0100

    oeoeoeoeooeoe

--- test.c
+++ test.c
@@ -6,29 +6,60 @@
 /*   By: sokaraku <sokaraku@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 17:06:36 by sokaraku          #+#    #+#             */
-/*   Updated: 2024/02/01 13:53:29 by sokaraku         ###   ########.fr       */
+/*   Updated: 2024/02/05 14:26:56 by sokaraku         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+// void	test(int *id)
+// {
+// 	*id = fork();
+// 	printf("id in test is = %d\n", *id);
+// }
+
+// int	main(int ac, char **av, char **env)
+// {
+// 	int	id;
+// 	char *args[] = {"cat", "test.c", NULL};
+// 	(void)ac;
+// 	test(&id);
+// 	if (id == 0)
+// 		printf("tg dans le process %d\n", id);
+// 	else
+// 		printf("tg dans le process %d\n", id);
+// 	av++;
+// 	printf("%s\n", *(env + 2));
+// 	if (id == 0)
+// 		execve("/bin/ls", av, NULL);
+// 	else
+// 		execve("/bin/cat", args, NULL);
+// 	return (0);
+// }
+
+#include <stdio.h>
+#include <unistd.h>
+
 int	main(void)
 {
-	int	file_desc;
-	int	copy_desc;
+	int	pipex[2];
+	int	process_id;
+	char	str[10];
 
-	// open() returns a file descriptor file_desc to a
-	// the file "dup.txt" here"
-	file_desc = open("dup.txt", O_WRONLY | O_APPEND | O_CREAT);
-	if (file_desc < 0)
-		printf("Error opening the file\n");
-	// dup() will create the copy of file_desc as the copy_desc
-	// then both can be used interchangeably.
-	copy_desc = dup(file_desc);
-	// write() will write the given string into the file
-	// referred by the file descriptors
-	write(copy_desc, "This will be output to the file named dup.txt\n", 46);
-	write(file_desc, "This will also be output to the file named dup.txt\n",
-		51);
-	return (0);
+	str[5] = 0;
+	pipe(pipex);
+	process_id = fork();
+	if (process_id == 0)
+	{
+		close(pipex[0]);
+		write(pipex[1], "salut", 5);
+		close(pipex[1]);
+	}
+	else
+	{
+		read(pipex[0], str, 5);
+		close(pipex[0]);
+		close(pipex[1]);
+	}
+	printf("Process %d : string %s\n", process_id, str);
 }
